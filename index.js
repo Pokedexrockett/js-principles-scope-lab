867299128ec2f8f0988f3b30ec2c69b92d619120
Dexter <dex429.dc@gmail.com>
2021-09-19 20:58:15 -0400

    commit tree 716151b2998f431dc4d755ad24ea7740150562bd

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'john';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    globalThis.bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer (){
+    leastFavoriteCustomer = 'medow';
+}
+
+console.log(changeLeastFavoriteCustomer());
